0416f8ab949ad7017e2f9fdae146a1b4b57437ba
hishaamn <n.hishaam@gmail.com>
2016-05-01 18:08:38 +0400

    Sitecore LinqSratchPad
    Code of the LinqSratchPad extender.

---
+++ Sitecore.LinqScratchPadExtender/Sitecore.LinqScratchPadExtender/Sitecore/Admin/LinqScratchPadExtender.aspx
@@ -0,0 +1,139 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="LinqScratchPadExtender.aspx.cs" Inherits="Sitecore.LinqScratchPadExtender.LinqScratchPadExtender" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="Head1" runat="server">
+    <title>Linq to Sitecore ScratchPad</title>
+    <link href="/sitecore/shell/themes/standard/default/WebFramework.css" rel="Stylesheet" />
+    <link href="/sitecore/admin/Wizard/UpdateInstallationWizard.css" rel="Stylesheet" />
+
+    <script type="text/javascript" src="/sitecore/shell/Controls/lib/jQuery/jquery.js"></script>
+
+    <script type="text/javascript" src="/sitecore/shell/controls/webframework/webframework.js"></script>
+    <script type="text/javascript" src="../../sitecore/shell/Applications/Buckets/libs/jquery-linedtextarea/jquery-linedtextarea.js"></script>
+    <link href="../../sitecore/shell/Applications/Buckets/libs/jquery-linedtextarea/jquery-linedtextarea.css" type="text/css" rel="stylesheet" />
+
+
+</head>
+<body>
+    <img src="/sitecore/shell/themes/standard/Images/WebFramework/banner.png"/>
+    <form id="form1" class="wf-container" runat="server">
+        <div>
+            <div style="padding: 20px;">Enter your code snippet in the following text field.</div>
+            <div>
+                <div>
+                    <asp:TextBox ID="LinqQuery" runat="server" Class="lined" Height="659px" Width="891px" TextMode="MultiLine" Font-Size="11px">
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Web;
+    using Sitecore.ContentSearch.SearchTypes;
+    using Sitecore.Buckets.Extensions;
+    using Sitecore.Buckets.Interfaces;
+    using Sitecore.Buckets.Search;
+    using Sitecore.Buckets.Search.Tags;
+    using Sitecore.Configuration;
+    using Sitecore.ContentSearch;
+    using Sitecore.ContentSearch.Utilities;
+    using Sitecore.Data;
+    using Sitecore.Data.Fields;
+    using Sitecore.Data.Items;
+    using Sitecore.Globalization;
+    using Sitecore.SecurityModel;
+    using Sitecore.Sites;
+    using Sitecore.Web;
+    using Sitecore;
+    using System.ComponentModel;
+    using System.Diagnostics.CodeAnalysis;
+
+    using Sitecore.ContentSearch.Linq;
+    using Sitecore.ContentSearch.Linq.Common;
+
+    using Constants = Sitecore.Buckets.Util.Constants;
+    using ContentSearchManager = Sitecore.ContentSearch.ContentSearchManager;
+                     
+    namespace Test {
+                    
+       class Program {
+                        public static IEnumerable&lt;SearchResultItem&gt; Main(string str)
+                        {
+                            using (var context = ContentSearchManager.GetIndex(&quot;sitecore_master_index&quot;).CreateSearchContext())
+                            {
+                                return context.GetQueryable&lt;SearchResultItem&gt;().Take(10).ToList();
+                            }
+                        }
+                    }
+                  }
+                    
+        [PredefinedQuery("_templatename", ComparisonType.Contains, "sample")]
+        public class TryYourOwnClass {
+                    
+                    [IndexField("_name")]
+                    public string Name {get; set;}
+                    
+                    [IgnoreIndexFieldAttribute]
+                    public string DoNotMapMe { get; set;}
+                    }
+                    </asp:TextBox>
+                </div>
+            </div>
+
+            <div class="wf-footer">
+                <div style="padding: 10px;">Insert your dll name in the textbox below. Separate each dll by a ','. Example: example1.dll, example2.dll</div>
+                <asp:TextBox ID="AssemblyList" runat="server" Height="100px" Width="649px" TextMode="MultiLine" Font-Size="11px"></asp:TextBox>
+
+				<hr/>
+
+                <div style="padding: 10px;">Insert additional <b>system assembly</b> here. Separate each dll by a ','. Example: System.dll, System.Xml.dll.</div>
+				<div><b>Already Present: System.dll, System.Xml.dll, mscorlib.dll, System.Configuration.dll, System.Core.dll</b></div>
+                <asp:TextBox ID="SystemAssembly" runat="server" Height="100px" Width="649px" TextMode="MultiLine" Font-Size="11px"></asp:TextBox>
+				
+				<hr/>
+                
+                <div style="padding: 10px;">Insert the <b>parameter values</b> here. Separate each value by a ','.</div>
+				<asp:TextBox ID="MethodParameters" runat="server" Height="100px" Width="649px" TextMode="MultiLine" Font-Size="11px"></asp:TextBox>
+				
+				<hr/>
+                
+				<div style="padding: 10px;">Insert the method that needs to be executed here. <b>Example: Main</b></div>
+                <asp:TextBox ID="MethodStartUp" runat="server" Height="25px" Width="300px" Font-Size="11px" required></asp:TextBox>
+
+                <div style="padding: 10px;">Insert your Main Class including your namespace here. <b>Example: Test.Program</b></div>
+                <asp:TextBox ID="MainClass" runat="server" Height="25px" Width="300px" Font-Size="11px" required></asp:TextBox>
+            </div>
+
+            <div class="wf-footer">
+                <asp:Button ID="btnNext" Text="Execute Code" runat="server" OnClick="btnNext_Click" />
+            </div>
+            <div class="wf-footer">
+                <asp:Literal ID="Output" runat="server"></asp:Literal>
+                <asp:Label ID="Error" runat="server"></asp:Label>
+            </div>
+        </div>
+        <div>
+        </div>
+        <div class="wf-content" style="padding: 2em 100px 0 32px;">
+            <asp:Label ID="lblError" CssClass="Error" Visible="false" runat="server"></asp:Label>
+            <h1 runat="server" id="lblHeader"></h1>
+            <asp:GridView ID="GridResults" runat="server" BackColor="White" BorderColor="Gray" Width="800">
+                <AlternatingRowStyle BackColor="Silver" />
+                <HeaderStyle BackColor="Gray" />
+                <RowStyle Width="600px" />
+            </asp:GridView>
+
+        </div>
+
+    </form>
+</body>
+
+<script>
+    jQuery(function () {
+        jQuery(".lined").linedtextarea(
+            { selectedLine: 1 }
+        );
+    });
+</script>
+
+</html>
---
+++ Sitecore.LinqScratchPadExtender/Sitecore.LinqScratchPadExtender/Sitecore/Admin/LinqScratchPadExtender.aspx.cs
@@ -0,0 +1,186 @@
+﻿
+namespace Sitecore.LinqScratchPadExtender
+{
+    using System.CodeDom.Compiler;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Drawing;
+    using System.Globalization;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+    using System.Web;
+    using Microsoft.CSharp;
+    using System;
+
+    public partial class LinqScratchPadExtender : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public static Tuple<string, IEnumerable<object>> CompileAndRun(string code, string assemblyList, string systemAssembly, string parameters, string mainClass, string methodStartUp)
+        {
+            var options = new CompilerParameters
+            {
+                GenerateInMemory = true,
+                TreatWarningsAsErrors = false,
+                GenerateExecutable = false,
+                CompilerOptions = "/optimize"
+            };
+
+            var strArray1 = new[]
+            {
+                "System.dll",
+                "System.Xml.dll",
+                "mscorlib.dll",
+                "System.Configuration.dll",
+                "System.Core.dll"
+            };
+
+            var strArray2 = new[]
+            {
+                "Sitecore.Kernel.dll",
+                "Sitecore.Buckets.dll",
+                "Sitecore.ContentSearch.dll",
+                "Sitecore.ContentSearch.Linq.dll"
+            };
+
+            for (var index = 0; index < strArray2.Length; ++index)
+            {
+                strArray2[index] = Path.Combine(Path.GetDirectoryName(HttpContext.Current.Request.PhysicalApplicationPath + "bin\\"), strArray2[index]);
+            }
+
+            if (systemAssembly.Length > 0)
+            {
+                var sysAssembly = systemAssembly.Replace(" ", string.Empty).Split(',');
+
+                options.ReferencedAssemblies.AddRange(sysAssembly);
+            }
+
+            if (assemblyList.Length > 0)
+            {
+                var assemblyArray = assemblyList.Replace(" ", string.Empty).Split(',');
+
+                for (var index = 0; index < assemblyArray.Length; ++index)
+                {
+                    assemblyArray[index] = Path.Combine(Path.GetDirectoryName(HttpContext.Current.Request.PhysicalApplicationPath + "bin\\"), assemblyArray[index].Trim());
+                }
+
+                options.ReferencedAssemblies.AddRange(assemblyArray);
+            }
+
+            options.ReferencedAssemblies.AddRange(strArray2);
+            options.ReferencedAssemblies.AddRange(strArray1);
+
+            var compilerResults = new CSharpCodeProvider().CompileAssemblyFromSource(options, code);
+
+            if (compilerResults.Errors.HasErrors)
+            {
+                var str = compilerResults.Errors.Cast<CompilerError>()
+                    .Aggregate("Compile error: ", (current, compilerError) => current + "rn" + compilerError);
+
+                return new Tuple<string, IEnumerable<object>>(str, new List<object>());
+            }
+
+            ExploreAssembly(compilerResults.CompiledAssembly);
+
+            var module = compilerResults.CompiledAssembly.GetModules()[0];
+
+            var type = module.GetTypes().FirstOrDefault(x => x.FullName.Contains(mainClass));
+
+            if (type == null)
+            {
+                return null;
+            }
+               
+            var methodInfo = type.GetMethod(methodStartUp);
+
+            if (!(methodInfo != null))
+            {
+                return new Tuple<string, IEnumerable<object>>("0", new List<object>());
+            }
+
+            var methodParameters = methodInfo.GetParameters();
+
+            var stopwatch = new Stopwatch();
+
+            var parametersObject = new object[methodParameters.Length];
+
+            stopwatch.Start();
+
+            if (parameters.Length > 0)
+            {
+                var paramList = parameters.Replace(" ", string.Empty).Split(',');
+
+                for (var i = 0; i < methodParameters.Length; i++)
+                {
+                    var paramType = methodParameters[i].ParameterType;
+                        
+                    parametersObject[i] = Convert.ChangeType(paramList[i], paramType);
+                }
+
+                var enumerable = methodInfo.Invoke(null, parametersObject) as IEnumerable<object>;
+
+                stopwatch.Stop();
+
+                return new Tuple<string, IEnumerable<object>>(stopwatch.ElapsedMilliseconds.ToString(CultureInfo.InvariantCulture), enumerable);
+            }
+            else
+            {
+                var enumerable = methodInfo.Invoke(null, new object[]
+                {
+
+                }) as IEnumerable<object>;
+
+                stopwatch.Stop();
+
+                return new Tuple<string, IEnumerable<object>>(stopwatch.ElapsedMilliseconds.ToString(CultureInfo.InvariantCulture), enumerable);
+            }
+        }
+
+        protected void btnNext_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var tuple = CompileAndRun
+                    (
+                        this.LinqQuery.Text,
+                        this.AssemblyList.Text,
+                        this.SystemAssembly.Text,
+                        this.MethodParameters.Text,
+                        this.MainClass.Text,
+                        this.MethodStartUp.Text
+                    );
+
+                this.GridResults.DataSource = tuple.Item2;
+                this.GridResults.DataBind();
+                this.Output.Text = tuple.Item1 + "ms to run";
+                this.Error.Text = string.Empty;
+            }
+            catch (Exception ex)
+            {
+                this.Error.ForeColor = Color.Red;
+                this.Error.Text = string.Format("Message: {0}\n, InnerMessage: {1}\n, StackTrace: {2}", ex.Message, ex.InnerException, ex.StackTrace);
+            }
+        }
+
+        private static void ExploreAssembly(Assembly assembly)
+        {
+            Console.WriteLine("Modules in the assembly:");
+            foreach (Module module in assembly.GetModules())
+            {
+                Console.WriteLine("{0}", module);
+                foreach (Type type in module.GetTypes())
+                {
+                    Console.WriteLine("t{0}", type.Name);
+                    foreach (var memberInfo in type.GetMethods())
+                    {
+                        Console.WriteLine("tt{0}", memberInfo.Name);
+                    }
+                }
+            }
+        }
+    }
+}
